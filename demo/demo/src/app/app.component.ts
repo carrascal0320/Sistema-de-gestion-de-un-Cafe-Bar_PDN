b7fbbd42a8dfb061bc56d016af95df8c224061fc
montydavid <jdmontaguthv@ufpso.edu.co>
2025-03-22 22:12:07 -0500

    SDGUCBP-7 <rutas>

--- demo/demo/src/app/app.component.ts
+++ demo/demo/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'springangular';
+  title = 'Sistema de gestion de un CafeBar';
+
+  constructor(private router: Router) {}
+
+  isAuthRoute(): boolean {
+    return this.router.url.includes('/login') || this.router.url.includes('/register');
+  }
 }
